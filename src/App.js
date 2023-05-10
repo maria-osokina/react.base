2550cdec2b34eb7fdb8eae8bbe00a0e33df0747d
Maria Osokina <osokina.m@medindex.ru>
2023-05-10 20:47:20 +0300

    controlled input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [value, setValue] = useState('Текст в инпуте')
 
   function increment() {
     setCount(count+1)
@@ -14,6 +15,8 @@ function App() {
   return (
     <div className="App">
       <h1>{count}</h1>
+      <h2>{value}</h2>
+      <input type="text" value={value} onChange={(event)=>setValue(event.target.value)} />
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
     </div>
