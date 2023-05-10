30505981214638e720b3cc99f188c52c628a4e4a
Maria Osokina <osokina.m@medindex.ru>
2023-05-10 20:51:38 +0300

    functional component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
+import Counter from "./components/Counter";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [value, setValue] = useState('Текст в инпуте')
-
-  function increment() {
-    setCount(count+1)
-  }
-
-  function decrement() {
-    setCount(count-1)
-  }
-
   return (
     <div className="App">
-      <h1>{count}</h1>
-      <h2>{value}</h2>
-      <input type="text" value={value} onChange={(event)=>setValue(event.target.value)} />
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <Counter />
+      <Counter />
+      <Counter />
     </div>
   );
 }
