d663bdc80b925ef627a45ac866cb0cc8911751da
Maria Osokina <osokina.m@medindex.ru>
2023-05-10 20:41:29 +0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count+1)
+  }
+
+  function decrement() {
+    setCount(count-1)
+  }
+
   return (
     <div className="App">
+      <h1>{count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
