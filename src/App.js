98a720ae90afb0191f60390c6304fabc87e9d942
Maria Osokina <osokina.m@medindex.ru>
2023-05-10 21:05:24 +0300

    class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
-      <Counter />
-      <Counter />
+      <ClassCounter />
     </div>
   );
 }
