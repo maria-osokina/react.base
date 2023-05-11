c5daa8d49d8cb8bfa8831a692f182c8e7214a185
Maria Osokina <osokina.m@medindex.ru>
2023-05-11 10:04:55 +0300

    styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
-import ClassCounter from "./components/ClassCounter";
+// import ClassCounter from "./components/ClassCounter";
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <ClassCounter />
+      <div className="post">
+        <div className="post__content">
+          <strong>1. Javascript</strong>
+          <div>Javascript - это язык программирования</div>
+        </div>
+        <div className="post__btns">
+          <button>Удалить</button>
+        </div>
+      </div>
+      {/* <ClassCounter /> */}
     </div>
   );
 }
