1faaf8765445f4c46ef3339d38473d69a387992f
Maria Osokina <osokina.m@medindex.ru>
2023-05-17 10:35:21 +0300

    Список постов

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from "react";
-// import ClassCounter from "./components/ClassCounter";
+import React, { useState } from "react";
 import './styles/App.css';
-import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript', body: 'Description' },
+    { id: 2, title: 'Typescript', body: 'Description' },
+    { id: 3, title: 'React', body: 'Description' }
+  ])
+
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: 'Python 1', body: 'Description' },
+    { id: 2, title: 'Python 2', body: 'Description' },
+    { id: 3, title: 'Python 3', body: 'Description' }
+  ])
+
   return (
     <div className="App">
-      <PostItem post={{ id: 1, title: 'Javascript', body: 'Description' }} />
-      <PostItem post={{ id: 2, title: 'Typescript', body: 'Description' }} />
-      <PostItem post={{ id: 3, title: 'React', body: 'Description' }} />
-
-      {/* <ClassCounter /> */}
+      <PostList posts={posts} title="Посты про JS" />
+      <PostList posts={posts2} title="Посты про Python" />
     </div>
   );
 }
