ffe33a4b8a9f2987914b19700bbce39ce8079417
Maria Osokina <osokina.m@medindex.ru>
2023-05-18 10:50:51 +0300

    Удаление поста

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import PostItem from "./PostItem";
 
-const PostList = ({ posts, title }) => {
+const PostList = ({ posts, title, remove }) => {
     return (
         <div>
             <h1 style={{ textAlign: "center" }}>{title}</h1>
-            {posts.map((post,index) => <PostItem post={post} number={index+1} key={post.id} />)}
+            {posts.map((post, index) =>
+                <PostItem
+                    post={post}
+                    number={index + 1}
+                    key={post.id}
+                    remove={remove}
+                />)}
         </div>
     );
 };
