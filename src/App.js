fc738e3824e085f7f8dcc1f42ddb1dde096d04a4
Maria Osokina <osokina.m@medindex.ru>
2023-05-18 10:46:17 +0300

    Обмен данными между компонентами

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import './styles/App.css';
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
 
 
 function App() {
@@ -12,33 +11,13 @@ function App() {
     { id: 3, title: 'React', body: 'Description' }
   ])
 
-  const [post, setPost] = useState({title: '', body: ''});
-
-  const addNewPost = (event) => {
-    event.preventDefault();
-    setPosts([...posts, {...post, id: Date.now()}]);
-    setPost({title: '', body: ''})
+  const createPost = (newPost) => {
+    setPosts([...posts, {...newPost}]);
   }
 
   return (
     <div className="App">
-      <form>
-        {/* Управляемый компонент */}
-        <MyInput
-          type="text"
-          placeholder="Название поста"
-          value={post.title}
-          onChange={(e) => setPost({ ...post, title: e.target.value })}
-        />
-        {/* Неуправляемый / Неконтролируемый компонент */}
-        <MyInput
-          type="text"
-          placeholder="Описание поста"
-          value={post.body}
-          onChange={(e) => setPost({ ...post, body: e.target.value })}
-        />
-        <MyButton onClick={addNewPost}>Создать пост</MyButton>
-      </form>
+      <PostForm create={createPost} />
       <PostList posts={posts} title="Посты про JS" />
     </div>
   );
